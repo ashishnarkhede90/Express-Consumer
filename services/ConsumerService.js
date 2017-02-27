0b2b99d7ff639ac34f52f6c8078c4ca7e281c262
Ashish Narkhede <ashish.narkhede90@gmail.com>
2017-02-26 17:33:39 -0800

    updating path logic in OAuthService

--- services/ConsumerService.js
+++ services/ConsumerService.js
@@ -9,12 +9,7 @@ var SobjectsUtil = require('../util/SobjectsUtil');
 dotEnv.load();
 
 const path = '/services/data/v38.0';
-
-console.log(process.env);
-
-if(process.env.INSTANCE_URL) {
-	const instanceUrl = process.env.INSTANCE_URL.replace('https://', '');
-}
+const instanceUrl = process.env.INSTANCE_URL.replace('https://', '');
 
 function InvalidSessionError(name, message) {
 	this.name = name;
